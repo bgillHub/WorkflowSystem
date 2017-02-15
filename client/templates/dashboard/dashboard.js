ec235cf27dc0c09e40c6253b0806bcad7f5ef927
Matthew Cucuzza <mattcucuzza@gmail.com>
2017-02-15 11:47:38 -0500

    fixed bug in dashboard.js

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,22 +1,18 @@
 Template.dashboardPage.events({
-    'click #createStateButton': function(e){
-      e.preventDefault();
-      console.log("You pressed Go To Create State button");
-      Router.go("/createState");
-<<<<<<< HEAD
-    },
-    'click #createTransButton': function(e){
-      e.preventDefault();
-      console.log("You pressed Go To Create Transition button");
-      Router.go("/createTrans");
-    },
-    'click .viewWorkflowArea': function(e) {
-      e.preventDefault();
-      console.log("You pressed the view button");
-      console.log(StatesList.find().fetch());
-      Router.go("/modifyWorkflow");
-    },
-=======
-    }
->>>>>>> 9758ff05df16406ee208c58871d86a3fb17e3fdf
+  'click #createStateButton': function(e){
+    e.preventDefault();
+    console.log("You pressed Go To Create State button");
+    Router.go("/createState");
+  },
+  'click #createTransButton': function(e){
+    e.preventDefault();
+    console.log("You pressed Go To Create Transition button");
+    Router.go("/createTrans");
+  },
+  'click .viewWorkflowArea': function(e) {
+    e.preventDefault();
+    console.log("You pressed the view button");
+    console.log(StatesList.find().fetch());
+    Router.go("/modifyWorkflow");
+  }
 });
