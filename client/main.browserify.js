040004063a3c4d00331a3f9136ea0ecc986b12f6
brian <brianrobgill@gmail.com>
2017-02-14 21:39:59 -0500

    Merging with createState

--- client/main.browserify.js
+++ client/main.browserify.js
@@ -2,10 +2,23 @@ if (Meteor.isClient) {
   var state = require('state.js');
   var model = new state.StateMachine("model");
   var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
-  var stateA = new state.State("stateA", model);
-  var stateB = new state.State("stateB", model);
-  console.log("HI CLIENT BRIAN!!!! We made states");
-}
+  var stateA = new state.State("stateOne", model);
+  var stateB = new state.State("stateTwo", model);
+  console.log("HI CLIENT BRIAN!!!! We made states " + stateA.name + ", " + stateB.name);
+  Meteor.startup(function () {
+    Meteor.methods({
+      createState: function (){
+        var state = require('state.js');
+        var model = new state.StateMachine("model");
+        var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
+        var stateC = new state.State("stateOld", model);
+        var stateD = new state.State("stateNew", model);
+        console.log("States Created: " + stateC.name + ", " + stateD.name);
+      }//end create state
+    });//end methods
+    console.log("HI BRIAN");
+  });//end startup
+}// if is client
 if (Meteor.isServer) {
   Meteor.startup(function () {
     Meteor.methods({
@@ -13,8 +26,8 @@ if (Meteor.isServer) {
         var state = require('state.js');
         var model = new state.StateMachine("model");
         var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
-        var stateA = new state.State("stateA", model);
-        var stateB = new state.State("stateB", model);
+        var stateA = new state.State("stateOld", model);
+        var stateB = new state.State("stateNew", model);
         console.log("State Created");
       }//end create state
     });//end methods
