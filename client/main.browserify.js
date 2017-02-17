f0256c706c959ba603714f6649089600f16ab067
Matthew Cucuzza <mattcucuzza@gmail.com>
2017-02-17 12:25:25 -0500

    added browserify

---
+++ client/main.browserify.js
@@ -0,0 +1,53 @@
+if (Meteor.isClient) {
+  Meteor.startup(function () {
+    var state = require('state.js');
+    state.setConsole(console);
+    model = new state.StateMachine("model");
+    console.log("Meteor Executed Client Code. Created StateMachine");
+    console.log("Meteor started as Client with Browserify");
+  });//end startup
+}// if is client
+if (Meteor.isServer) {
+  Meteor.methods({
+    'saveWorkflow': function (Name){
+      var state = require('state.js');
+      state.setConsole(console);
+      var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
+      var stateNew = new state.State(Name, model);
+      initial.to(stateNew);
+      var instance = new state.StateMachineInstance("State.js Class Representative");
+      state.initialise(model, instance);
+      var jsonObject = new state.JSONInstance("JSON Representative");
+      state.initialise(model, jsonObject);
+      jsonString = jsonObject.toJSON();
+      if (jsonString){
+        console.log("Created Json Passer");
+      }
+      else {console.log("JSON instance found empty");}
+      console.log("State Created on Client Method: " + stateNew.name);
+      console.log("Workflows:" + Workflows.find().fetch());
+      console.log("Workflow Name: " + Workflows.findOne().workflowName);
+      console.log("Workflow Saved On Client Method");
+    }//end create state
+  });//end methods
+
+}//end is server
+Meteor.methods({
+  'saveWorkflow': function (Name){
+    var state = require('state.js');
+    state.setConsole(console);
+    var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
+    var stateNew = new state.State(Name, model);
+    initial.to(stateNew);
+    var instance = new state.StateMachineInstance("progress");
+    state.initialise(model, instance);
+    var jsonObject = new state.JSONInstance("jsonInstane");
+    state.initialise(model, jsonObject);
+    jsonString = jsonObject.toJSON();
+    if (jsonString){
+      console.log("Created Json Passer");
+    }
+    else {console.log("JSON instance found empty");}
+    console.log("State Created on Client Method: " + stateNew.name);
+  }//end create state
+});//end methods
