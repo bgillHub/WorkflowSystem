af419f56189142d1fc350a720db6ac25ca86163c
Matthew Cucuzza <mattcucuzza@gmail.com>
2017-04-06 10:46:08 -0400

    pull

--- client/templates/createAccount/createAccount.js
+++ client/templates/createAccount/createAccount.js
@@ -1,14 +1,27 @@
 Template.createAccount.events({
-  'click .createButton': function(e) {
-    var firstName = document.getElementById("firstNameField").value;
-    var lastName = document.getElementById("lastNameField").value;
-    var email = document.getElementById("emailField").value;
-    var password = document.getElementById("passwordField").value;
+  // 'click .createButton': function(e) {
+  //   var firstName = document.getElementById("firstNameField").value;
+  //   var lastName = document.getElementById("lastNameField").value;
+  //   var email = document.getElementById("emailField").value;
+  //   var password = document.getElementById("passwordField").value;
+  //   Accounts.createUser({
+  //     firstName: firstName,
+  //     lastName: lastName,
+  //     email: email,
+  //     password: password
+  //   });
+  // }
+  'submit form': function(event) {
+    event.preventDefault();
+    var emailVar = event.target.signupEmail.value;
+    var passwordVar = event.target.signupPassword.value;
+    var nameVar = event.target.nameField.value;
     Accounts.createUser({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password
+      email: emailVar,
+      password: passwordVar,
+      profile: {
+        name: nameVar
+      }
     });
   }
 });
